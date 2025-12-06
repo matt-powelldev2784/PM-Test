b861bbf544a6dd2d476e44edca1b15bc5ec4dec2
Matthew Powell <matt.powell2784@gmail.com>
2025-12-06 11:29:12 +0000

    feat: add forex carousel to hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import Header from '@/components/header/header'
 import Hero from '@/components/hero/hero'
-import ForexCarousel from '@/components/ForexCarousel/ForexCarousel'
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
-      <ForexCarousel />
     </>
   )
 }
--- src/components/hero/forexData.ts
+++ src/components/hero/forexData.ts
@@ -1,4 +1,4 @@
-export const data = [
+export const forexData = [
   {
     key: 1,
     pair: 'USD/EUR',
